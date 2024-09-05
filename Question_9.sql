2fcd53fc3bc309936ec907b94dba8071c2659879
Azeela <azeela.marecar@etu.u-pec.fr>
2024-09-05 12:55:48 +0200

    modif

--- Question_9.sql
+++
@@ -1,6 +0,0 @@
-SELECT p.patient_id, p.nom, p.prenom, AVG(JULIANDAY(c2.date_consultation) - JULIANDAY(c1.date_consultation)) AS duree_moyenne
-FROM consultations c1
-JOIN consultations c2 ON c1.patient_id = c2.patient_id AND c2.date_consultation > c1.date_consultation
-JOIN patients p ON c1.patient_id = p.patient_id
-GROUP BY p.patient_id
-HAVING COUNT(c1.consultation_id) > 1;
